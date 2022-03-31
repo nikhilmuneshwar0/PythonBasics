33e0ac077d6569e32c13adddcf628f7fb5b56062
nikhilmuneshwa1 <96412735+nikhilmuneshwar0@users.noreply.github.com>
2022-03-31 14:38:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Data Types
+city = "Abbey Road"
+print(city[4]+city[7])
+
+name = input("What is your name?\n")
+lenName = len(name)#taking the length
+lenName1 = str(lenName)#typeCasting into string.
+print("Your name has " + lenName1 + " characters in it!")
+
+
